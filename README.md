a67364eec7b6ee572194ba6a9e0862b575182b46
elase huang <elase.huang@outlook.com>
2023-03-27 01:37:08 +0800

    🧼refactor: simple tilemap demo

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
 # readme
 
 a re-implement of enter the gungeon with bevy engine
+
+1. use matrix represent each level tiles, for example:
+    ```rust
+    fn main() {
+        [
+            1, 1, 1, 1, 1, 1, 1, 1, 1,
+            0, 0, 0, 0, 0, 0, 1, 0, 0,
+            1, 0, 0, 0, 0, 0, 1, 0, 1,
+            1, 0, 0, 0, 0, 1, 1, 0, 1,
+            1, 0, 1, 0, 0, 0, 0, 0, 1,
+            0, 0, 1, 0, 0, 0, 0, 0, 1,
+            1, 0, 1, 0, 0, 0, 0, 0, 1,
+            1, 1, 1, 1, 1, 1, 1, 1, 1,
+        ];
+    }
+    ```
+   0 represent the floor and 1 represent the wall, of course the floor usually have multi-style.
+2. each level should have multi gate that used to enter and exit. and combination with other level also need this
+   information.
--- src/main.rs
+++ src/main.rs
@@ -5,5 +5,57 @@ fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_plugin(WorldInspectorPlugin::new())
+        .add_startup_system(setup)
         .run();
+}
+
+fn setup(
+    mut c: Commands,
+    asset_server: Res<AssetServer>,
+    mut texture_atlases: ResMut<Assets<TextureAtlas>>,
+) {
+    c.spawn(Camera2dBundle::default());
+
+    let texture_altlas = TextureAtlas::from_grid(
+        asset_server.load("art/floor.png"),
+        Vec2::new(16.0, 16.0),
+        10,
+        2,
+        None,
+        None,
+    );
+    let texture_altlas_handle = texture_atlases.add(texture_altlas);
+
+    #[rustfmt::skip]
+    let level = [
+        1, 1, 1, 1, 1, 1, 1, 1, 1,
+        0, 0, 0, 0, 0, 0, 1, 0, 0,
+        1, 0, 0, 0, 0, 0, 1, 0, 1,
+        1, 0, 0, 0, 0, 1, 1, 0, 1,
+        1, 0, 1, 0, 0, 0, 0, 0, 1,
+        0, 0, 1, 0, 0, 0, 0, 0, 1,
+        1, 0, 1, 0, 0, 0, 0, 0, 1,
+        1, 1, 1, 1, 1, 1, 1, 1, 1,
+    ];
+
+    for x in 0..9 {
+        for y in 0..8 {
+            let index = y * 9 + x;
+            let tile = level[index];
+            if tile != 1 {
+                c.spawn(SpriteSheetBundle {
+                    sprite: TextureAtlasSprite {
+                        index: 0,
+                        ..default()
+                    },
+                    texture_atlas: texture_altlas_handle.clone(),
+                    transform: Transform {
+                        translation: Vec3::new(x as f32 * 16.0, y as f32 * 16.0, 0.0),
+                        ..default()
+                    },
+                    ..default()
+                });
+            }
+        }
+    }
 }
