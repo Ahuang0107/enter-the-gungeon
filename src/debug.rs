f13041f7ff76dd5c16b5fd87143794a34f19623d
elase huang <elase.huang@outlook.com>
2023-04-01 01:27:27 +0800

    🧼refactor: create debug plugin

---
+++ src/debug.rs
@@ -0,0 +1,66 @@
+use bevy::input::mouse::MouseWheel;
+use bevy::prelude::*;
+use bevy_inspector_egui::quick::WorldInspectorPlugin;
+use bevy_screen_diagnostics::{ScreenDiagnosticsPlugin, ScreenFrameDiagnosticsPlugin};
+
+pub struct DebugPlugin;
+
+impl Plugin for DebugPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_plugin(WorldInspectorPlugin::new())
+            .add_plugin(ScreenDiagnosticsPlugin::default())
+            .add_plugin(ScreenFrameDiagnosticsPlugin)
+            .add_system(move_camera)
+            .add_system(scroll_camera);
+    }
+}
+
+fn move_camera(
+    mut camera_current: Local<Vec2>,
+    mut camera_target: Local<Vec2>,
+    mut query_cameras: Query<&mut Transform, With<Camera2d>>,
+    keyboard: Res<Input<KeyCode>>,
+) {
+    let speed = 10.0;
+
+    if keyboard.pressed(KeyCode::W) {
+        camera_target.y += speed;
+    }
+    if keyboard.pressed(KeyCode::S) {
+        camera_target.y -= speed;
+    }
+    if keyboard.pressed(KeyCode::A) {
+        camera_target.x -= speed;
+    }
+    if keyboard.pressed(KeyCode::D) {
+        camera_target.x += speed;
+    }
+
+    let blend_ratio = 0.18;
+    let movement = *camera_target - *camera_current;
+    *camera_current += movement * blend_ratio;
+
+    for mut camera_transform in query_cameras.iter_mut() {
+        camera_transform.translation.x = camera_current.x;
+        camera_transform.translation.y = camera_current.y;
+    }
+}
+
+fn scroll_camera(
+    mut scroll_evr: EventReader<MouseWheel>,
+    keyboard_input: Res<Input<KeyCode>>,
+    mut query: Query<&mut OrthographicProjection>,
+) {
+    if keyboard_input.pressed(KeyCode::LShift) {
+        for ev in scroll_evr.iter() {
+            for mut op in query.iter_mut() {
+                op.scale += ev.y / 1000.0;
+                if op.scale < 0.1 {
+                    op.scale = 0.1;
+                } else if op.scale > 1.0 {
+                    op.scale = 1.0;
+                }
+            }
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
 use bevy::prelude::*;
 use bevy::render::{RenderApp, RenderSet};
-use bevy_inspector_egui::quick::WorldInspectorPlugin;
-use bevy_screen_diagnostics::{ScreenDiagnosticsPlugin, ScreenFrameDiagnosticsPlugin};
 
 mod custom_material;
 mod custom_mesh;
+mod debug;
 mod render_set_system;
 mod tilemap;
 
@@ -12,14 +11,11 @@ fn main() {
     let mut app = App::new();
 
     app.add_plugins(DefaultPlugins)
-        .add_plugin(WorldInspectorPlugin::new())
-        .add_plugin(ScreenDiagnosticsPlugin::default())
-        .add_plugin(ScreenFrameDiagnosticsPlugin)
+        .add_plugin(debug::DebugPlugin)
         .add_startup_system(setup_camera)
         .add_startup_system(tilemap::setup)
         .add_startup_system(custom_mesh::setup)
-        .add_startup_system(custom_material::setup)
-        .add_system(system_move_camera);
+        .add_startup_system(custom_material::setup);
 
     let render_app = app.sub_app_mut(RenderApp);
     render_app.add_system(render_set_system::setup.in_set(RenderSet::ExtractCommands));
@@ -29,37 +25,4 @@ fn main() {
 
 fn setup_camera(mut c: Commands) {
     c.spawn(Camera2dBundle::default());
-}
-
-fn system_move_camera(
-    mut camera_current: Local<Vec2>,
-    mut camera_target: Local<Vec2>,
-    mut query_cameras: Query<&mut Transform, With<Camera2d>>,
-    keyboard: Res<Input<KeyCode>>,
-) {
-    let speed = 10.0;
-
-    if keyboard.pressed(KeyCode::W) {
-        camera_target.y += speed;
-    }
-    if keyboard.pressed(KeyCode::S) {
-        camera_target.y -= speed;
-    }
-    if keyboard.pressed(KeyCode::A) {
-        camera_target.x -= speed;
-    }
-    if keyboard.pressed(KeyCode::D) {
-        camera_target.x += speed;
-    }
-
-    // Smooth camera.
-    let blend_ratio = 0.18;
-    let movement = *camera_target - *camera_current;
-    *camera_current += movement * blend_ratio;
-
-    // Update all sprite cameras.
-    for mut camera_transform in query_cameras.iter_mut() {
-        camera_transform.translation.x = camera_current.x;
-        camera_transform.translation.y = camera_current.y;
-    }
 }
